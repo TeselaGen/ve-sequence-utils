4ffecc86bd061145e5c8455b6122ebbe9f55fdad
Thomas Rich <trich1230@gmail.com>
2016-06-21 14:49:28 -0700

    adding teselagen feature colors map

---
+++ featureColors.js
@@ -0,0 +1,11 @@
+module.exports = {
+  default: "#CCCCCC",
+  promoter: "#31B440",
+  terminator: "#F51600",
+  cds: "#EF6500",
+  m_rna: "#FFFF00",
+  misc_binding: "#006FEF",
+  misc_part: "#006FEF",
+  misc_marker: "#8DCEB1",
+  rep_origin: "#878787"
+}
