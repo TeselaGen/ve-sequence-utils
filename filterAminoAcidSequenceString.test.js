390ceff1c9d89bc1b14832a109dd0732c0891e84
Thomas Rich <trich1230@gmail.com>
2016-08-22 09:12:32 -0700

    adding another filterAminoAcidSequenceString test case

--- filterAminoAcidSequenceString.test.js
+++ filterAminoAcidSequenceString.test.js
@@ -5,6 +5,10 @@ describe('filterAminoAcidSequenceString', function() {
         var filteredString = filterAminoAcidSequenceString('bbb342"""xtgalmfwkqespvicyhrnd,,../')
         assert.equal(filteredString, 'xtgalmfwkqespvicyhrnd')
     })
+    it('should handle upper case letters', function() {
+        var filteredString = filterAminoAcidSequenceString('xtgalmfWKQEspvicyhrnd')
+        assert.equal(filteredString, 'xtgalmfWKQEspvicyhrnd')
+    })
     it('should handle the option to includeStopCodon by allowing periods', function() {
         var options = {includeStopCodon: true}
         var filteredString = filterAminoAcidSequenceString('bbb342"""xtgalmfwkqespvicyhrnd,,../', options)
