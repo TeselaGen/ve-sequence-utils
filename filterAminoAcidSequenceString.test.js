927747054b67037bc544883257a0e375f5d272d4
Thomas Rich <trich1230@gmail.com>
2016-08-22 09:10:47 -0700

    adding test for filterAminoAcidSequenceString

---
+++ filterAminoAcidSequenceString.test.js
@@ -0,0 +1,13 @@
+var assert = require('assert')
+var filterAminoAcidSequenceString = require('./filterAminoAcidSequenceString')
+describe('filterAminoAcidSequenceString', function() {
+    it('should filter only valid amino acids by default', function() {
+        var filteredString = filterAminoAcidSequenceString('bbb342"""xtgalmfwkqespvicyhrnd,,../')
+        assert.equal(filteredString, 'xtgalmfwkqespvicyhrnd')
+    })
+    it('should handle the option to includeStopCodon by allowing periods', function() {
+        var options = {includeStopCodon: true}
+        var filteredString = filterAminoAcidSequenceString('bbb342"""xtgalmfwkqespvicyhrnd,,../', options)
+        assert.equal(filteredString, 'xtgalmfwkqespvicyhrnd..')
+    })
+})
