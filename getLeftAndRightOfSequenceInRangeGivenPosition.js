1f2d83bd224faca389c0604fbb6a07aa1c1a0646
Thomas Rich <trich1230@gmail.com>
2016-09-30 18:01:17 -0700

    updating getRecognitionSiteLeftAndRightAfterCut to be a more generalized function getLeftAndRightOfSequenceInRangeGivenPosition

---
+++ getLeftAndRightOfSequenceInRangeGivenPosition.js
@@ -0,0 +1,22 @@
+var isPositionWithinRange = require('ve-range-utils/isPositionWithinRange')
+var getSequenceWithinRange = require('ve-range-utils/getSequenceWithinRange')
+var normalizePositionByRangeLength = require('ve-range-utils/normalizePositionByRangeLength')
+var isPositionCloserToRangeStartThanRangeEnd = require('ve-range-utils/isPositionCloserToRangeStartThanRangeEnd')
+
+module.exports = function getLeftAndRightOfSequenceInRangeGivenPosition(range, position, sequence, sequenceLength) {
+  var result = {
+    leftHandSide: '',
+    rightHandSide: '',
+  }
+  if (isPositionWithinRange(position, range)) {
+    result.leftHandSide = getSequenceWithinRange({start: range.start, end: normalizePositionByRangeLength(position-1, sequence.length)}, sequence)
+    result.rightHandSide = getSequenceWithinRange({start:position, end: range.end}, sequence)
+  } else {
+    if (isPositionCloserToRangeStartThanRangeEnd(position, range, sequenceLength)) {
+      result.rightHandSide = getSequenceWithinRange(range, sequence)
+    } else {
+      result.leftHandSide = getSequenceWithinRange(range, sequence)
+    }
+  }
+  return result
+}
