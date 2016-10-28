07d72d58ab804d91b9abaada6b039e363bad0718
Thomas Rich <trich1230@gmail.com>
2016-10-27 22:50:09 -0700

    updating dna complement map to include all bases included in teselagen's app

--- DNAComplementMap.js
+++ DNAComplementMap.js
@@ -7,6 +7,22 @@ var DNAComplementMap = {
     T: 'A',
     C: 'G',
     G: 'C',
+    r: 'y',
+    R: 'Y',
+    y: 'r',
+    Y: 'R',
+    d: 'h',
+    D: 'H',
+    h: 'd',
+    H: 'D',
+    k: 'm',
+    K: 'M',
+    m: 'k',
+    M: 'K',
+    v: 'b',
+    V: 'B',
+    b: 'v',
+    B: 'V',
     //tnrtodo add more letters here
 };
 
