133541d25bf168fdb04d2c6bd94c985acaceace9
Thomas Rich <tnrich@teselagen.com>
2018-05-30 13:13:03 -0700

    slightly changing tidyUpAnnotation call

--- src/tidyUpAnnotation.js
+++ src/tidyUpAnnotation.js
@@ -4,7 +4,7 @@ const areNonNegativeIntegers = require("validate.io-nonnegative-integer-array");
 const bsonObjectid = require('bson-objectid')
 
 module.exports = function cleanUpAnnotation(annotation, {
-  seqData={},
+  sequenceData={},
   annotationType,
   provideNewIdsForAnnotations,
   messages=[]
