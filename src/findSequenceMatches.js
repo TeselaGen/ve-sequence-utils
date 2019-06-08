ee7550417166911340e45e6aca3327f0f8546990
Eric Sturman <eric.sturman@gmail.com>
2019-06-08 07:44:10 -0500

    Update findSequenceMatches.js

--- src/findSequenceMatches.js
+++ src/findSequenceMatches.js
@@ -25,7 +25,6 @@ module.exports = function findSequenceMatches(
       searchString,
       options
     );
-
     const flippedReverseMatches = reverseMatches.map(range => {
       return {
         ...flipContainedRange(
