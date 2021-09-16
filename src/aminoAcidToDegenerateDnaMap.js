31e19e12583f02166dab5da7b5662f1859121491
XingGao-PKI <xing.gao@perkinelmer.com>
2021-09-16 11:39:01 +0800

    update

--- src/aminoAcidToDegenerateDnaMap.js
+++ src/aminoAcidToDegenerateDnaMap.js
@@ -3,28 +3,30 @@ const aminoAcidToDegenerateDnaMap = {
   ".": "...",
   "*": "trr",
   a: "gcn",
-  b: "ray",
-  c: "ugy",
+  b: "ray", // D or N => aay + gay = ray
+  c: "tgy",
   d: "gay",
   e: "gar",
-  f: "uuy",
+  f: "tty",
   g: "ggn",
   h: "cay",
-  i: "auh",
+  i: "ath",
+  j: "htn", // L or I ytn + ath => htn
   k: "aar",
-  l: "yun",
-  m: "aug",
+  l: "ytn", // YTR、CTN => Y=CT, N=AGCT
+  m: "atg",
   n: "aay",
+  o: "tag", // Pyrrolysine, https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2933860/
   p: "ccn",
   q: "car",
-  r: "mng",
-  s: "wsn",
+  r: "mgn", // CGN、MGR => M=AC, N=AGCT
+  s: "wsn", // TCN、AGY => AT = w, CG = S, N is AGCT
   t: "acn",
-  u: "uga",
-  v: "gun",
-  w: "ugg",
-  x: "nnn",
-  y: "uay",
-  z: "sar"
+  u: "tga", // Selenocysteine, https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2933860/
+  v: "gtn",
+  w: "tgg",
+  x: "nnn", // unknown aa.
+  y: "tay",
+  z: "sar" // E or Q, => gar + car = sar
 };
 module.exports = aminoAcidToDegenerateDnaMap;
